bb3589050a530cb8e61b12245068df81c19360c7
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-01-22 15:56:24 +1100

    Add some validation to answers

--- index.js
+++ index.js
@@ -9,22 +9,39 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is the title of your application?',
+        validate: function (title) {
+            if (title) {
+                return true
+            } else {
+                console.log('Please enter a title for your project')
+                return false
+            }
+        }
     },
     {
         type: 'input',
         name: 'description',
         message: 'In a sentence or two, describe the what, why and how of your project.',
+        validate: function (description) {
+            if (description) {
+                return true
+            } else {
+                console.log('Please enter a description of your project')
+                return false
+            }
+        }
     },
     {
         type: 'input',
         name: 'Installation',
-        message: 'Describe steps to install your application if applicable',
+        message: 'Describe steps to install your application. If none, press Enter.',
         default: 'N/A'
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Describe how to use your application'
+        message: 'Describe how to use your application. If none, press Enter.',
+        default: 'N/A'
     },
     {
         type: 'list',
@@ -36,24 +53,40 @@ const questions = [
     {
         type: 'input',
         name: 'credits',
-        message: 'List your collaborators if applicable.',
+        message: 'List your collaborators if applicable. If none, press Enter.',
         default: 'N/A'
     },
     {
         type: 'input',
         name: 'tests',
-        message: 'Describe any tests to determine your application is working as expected.'
+        message: 'Describe any tests to determine your application is working as expected. If none, press Enter.',
+        default: 'N/A'
     },
     {
         type: 'input',
         name: 'email',
-        message: 'Please enter your email address.',
-        validate: ''
+        message: 'Please enter your email address (required).',
+        validate: function (email) {
+            if (email) {
+                return true
+            } else {
+                console.log('Please enter a valid email address.')
+                return false
+            }
+        }
     },
     {
         type: 'input',
         name: 'github',
-        message: 'Please enter your GitHub username'
+        message: 'Please enter your GitHub username (required).',
+        validate: function (github) {
+            if (github) {
+                return true
+            } else {
+                console.log('Please enter a valid email address.')
+                return false
+            }
+        }
     }
     
 ];
@@ -65,7 +98,13 @@ inquirer.prompt(questions).then((response)=> {
 });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const filename = 'README.md'
+    
+    fs.writeFile(filename, data, err =>
+    err ? console.log(err) : console.log('Success!')
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
