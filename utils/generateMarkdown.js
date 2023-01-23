161644298a9ea1cebfc54ce70a8f8fb118aa4ede
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-01-23 14:20:11 +1100

    Add renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,14 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadge = license
+  if (licenseBadge) {
+    return `![License Badge](https://img.shields.io/badge/License-${licenseBadge}-success)`
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
