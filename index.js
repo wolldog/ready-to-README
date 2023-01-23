445911d6b9c31ec3e3aef80cbbb0bb606aba2973
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-01-24 10:24:27 +1100

    Comments: index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// TODO: Include packages needed for this application
+// Required packages: inquirer v8.2.4; fileSystem; locally exported function 'generateMarkdown'
 
 const inquirer = require("inquirer")
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+// Questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
         message: 'What is the title of your application?',
+        //Require user to provide a title
         validate: function (title) {
             if (title) {
                 return true
@@ -23,6 +24,7 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'In a sentence or two, describe the what, why and how of your project.',
+        //Require user to provide a title
         validate: function (description) {
             if (description) {
                 return true
@@ -36,12 +38,14 @@ const questions = [
         type: 'input',
         name: 'installation',
         message: 'Describe steps to install your application. If none, press Enter.',
+        //If user does not provide input, record N/A/
         default: 'N/A'
     },
     {
         type: 'input',
         name: 'usage',
         message: 'Describe how to use your application. If none, press Enter.',
+        //If user does not provide input, record N/A/
         default: 'N/A'
     },
     {
@@ -49,24 +53,28 @@ const questions = [
         name: 'license',
         message: 'Choose the license that applies to your project',
         choices: ['None', 'Apache_2', 'GNU_GPL_3', 'MIT'],
+        //If user does not provide input, record 'None'/
         default: 'None',
     },
     {
         type: 'input',
         name: 'credits',
         message: 'List your collaborators if applicable. If none, press Enter.',
+        //If user does not provide input, record N/A/
         default: 'N/A'
     },
     {
         type: 'input',
         name: 'tests',
         message: 'Describe any tests to determine your application is working as expected. If none, press Enter.',
+        //If user does not provide input, record N/A/
         default: 'N/A'
     },
     {
         type: 'input',
         name: 'email',
         message: 'Please enter your email address (required).',
+        //Require user to provide an email address
         validate: function (email) {
             if (email) {
                 return true
@@ -80,6 +88,7 @@ const questions = [
         type: 'input',
         name: 'github',
         message: 'Please enter your GitHub username (required).',
+        //Require user to provide a GitHub username.
         validate: function (github) {
             if (github) {
                 return true
@@ -93,16 +102,18 @@ const questions = [
 ];
 
 
-// TODO: Create a function to write README file
+// Function to create the README.md file or notify user of an error if unable to produce file.
 function writeToFile(fileName, data) {
     
     fs.writeFile(fileName, data, err =>
     err ? console.log(err) : console.log('You have successfully created a README.md file! You will find it in the "results" folder')
-
   );
 }
 
-// TODO: Create a function to initialize app
+// Function to intialise each step of the application process;
+// 1. Call inquirer.prompt to ask questions
+// 2. Call 'generateMarkdown' in generateMarkdown.js to produce content from responses
+// 3. Call writeToFile to create the README.md file
 function init() {
     inquirer.prompt(questions).then((response)=> {
         const readMeContent = generateMarkdown (response)
@@ -113,5 +124,5 @@ function init() {
 }
 
 
-// Function call to initialize app
+// Call the initialise function
 init();
